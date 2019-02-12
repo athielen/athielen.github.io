32f9732920fa12c64ee7b77e343458433166a629
athielen <anthony@anthonythielen.com>
2019-02-12 15:22:49 -0600

    updated local_startup script

--- local_startup.sh
+++ local_startup.sh
@@ -1,1 +1,2 @@
-jekyll serve --watch --config _config.yml,_config_dev.yml -p 9000
+# jekyll serve --watch --config _config.yml,_config_dev.yml -p 9000
+bundle exec jekyll serve --watch --config _config.yml,_config_dev.yml -p 9000
